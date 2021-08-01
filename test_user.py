7a239411dc0016fafca3f6e416bfcae92f6c056c
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-01 21:13:14 +0300

    test_init method for ensuring correct initialization

--- test_user.py
+++ test_user.py
@@ -6,9 +6,21 @@ class TestUser(unittest.TestCase):
         self.new_account = User("test_user","test_password") 
         
         
+    def test_init(self):
+        '''
+        Test case to test if object has initialized.
+        '''
+        
+        self.assertTrue(self.new_account.username,"test_user")
+        self.assertEqual(self.new_account.password,"test_password")
+        
     def tearDown(self):
         '''
         Method for cleaning up after other tests.
         '''
         
         User.user_list = []
+        
+        
+if __name__ == '__main__':
+    unittest.main()
